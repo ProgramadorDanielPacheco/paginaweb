42cd6a6689b0b68af6da33d669b4d00037d15a7c
Cheemspeon <danielpacheco842005@gmail.com>
2022-11-07 12:41:13 -0500

    Segundo Commit

--- paginaWebAngular/src/app/app-routing.module.ts
+++ paginaWebAngular/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
+import { Componente5Component } from './componente5/componente5.component';
+import { Componente6Component } from './componente6/componente6.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'componenete1', component: Componente1Component},
+  {path: 'componente2', component: Componente2Component},
+  {path: 'componente3', component: Componente3Component},
+  {path: 'componente4', component: Componente4Component},
+  {path: 'componente5', component: Componente5Component},
+  {path: 'componente6', component: Componente6Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- paginaWebAngular/src/app/app.component.ts
+++ paginaWebAngular/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
+import { Componente5Component } from './componente5/componente5.component';
+import { Componente6Component } from './componente6/componente6.component';
 
 @Component({
   selector: 'app-root',
