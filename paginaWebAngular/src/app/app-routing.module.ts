0050bff62f5dd2e07aa9e3e76774ac1190e4c86b
Cheespeon <danielpacheco842005@gmail.com>
2022-11-14 11:59:29 -0500

    Clase de navegacion

--- paginaWebAngular/src/app/app-routing.module.ts
+++ paginaWebAngular/src/app/app-routing.module.ts
@@ -3,18 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { Componente3Component } from './componente3/componente3.component';
-import { Componente4Component } from './componente4/componente4.component';
-import { Componente5Component } from './componente5/componente5.component';
-import { Componente6Component } from './componente6/componente6.component';
+
 
 
 const routes: Routes = [
-  {path: 'componenete1', component: Componente1Component},
-  {path: 'componente2', component: Componente2Component},
-  {path: 'componente3', component: Componente3Component},
-  {path: 'componente4', component: Componente4Component},
-  {path: 'componente5', component: Componente5Component},
-  {path: 'componente6', component: Componente6Component},
+  {path: '', component: Componente1Component},
+  {path: 'web2', component: Componente2Component},
+  {path: 'web3', component: Componente3Component},
+  {path: 'web1', component: Componente1Component}
 ];
 
 @NgModule({
